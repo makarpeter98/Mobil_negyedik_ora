e7cab41c8cd172f910ae458638948f8bcbe3396b
makarpeter <makar.peter98@gmail.com>
2025-03-11 14:38:57 +0100

    Intent commit

--- app/src/main/java/com/example/mobil_negyedik_ora/ItemsActivity.java
+++ app/src/main/java/com/example/mobil_negyedik_ora/ItemsActivity.java
@@ -1,6 +1,11 @@
 package com.example.mobil_negyedik_ora;
 
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ItemsActivity extends AppCompatActivity {
+
+    public void addItem(View view) {
+    }
 }
--- app/src/main/java/com/example/mobil_negyedik_ora/MainActivity.java
+++ app/src/main/java/com/example/mobil_negyedik_ora/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.mobil_negyedik_ora;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,19 +13,18 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView itemsTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
+       itemsTextView = findViewById(R.id.itemsTextView);
     }
 
     public void addButtonClicked(View view) {
+        Intent intent = new Intent(this, ItemsActivity.class);
+        startActivity(intent);
     }
 
     public void searchButtonClicked(View view) {

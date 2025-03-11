e51dce1986ddd404937541adb69cff5417cfe8ed
makarpeter <makar.peter98@gmail.com>
2025-03-11 14:41:34 +0100

    ItemsActivity onCreate kész

--- app/src/main/java/com/example/mobil_negyedik_ora/ItemsActivity.java
+++ app/src/main/java/com/example/mobil_negyedik_ora/ItemsActivity.java
@@ -1,10 +1,17 @@
 package com.example.mobil_negyedik_ora;
 
+import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_items);
+    }
 
     public void addItem(View view) {
     }
